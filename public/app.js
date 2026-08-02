cfa1e6a6749301b3ebe25af23414b63b5b7b0357
suddenrabbit <zhoumeng1987@gmail.com>
2026-08-02 18:41:07 +0800

    fix: streamline pwa startup

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const COLORS = ["violet", "mint", "orange", "blue", "rose"];
 const COLOR_NAMES = { violet: "葡萄紫", mint: "薄荷绿", orange: "日落橙", blue: "海盐蓝", rose: "莓果粉" };
-const APP_VERSION = "v20260802.000951";
-const EXPECTED_SERVICE_WORKER_VERSION = "rabbittodo-v55";
+const APP_VERSION = "v20260802.183945";
+const EXPECTED_SERVICE_WORKER_VERSION = "rabbittodo-v57";
 const SERVICE_WORKER_CHECK_INTERVAL = 10 * 60 * 1_000;
 const SERVICE_WORKER_RETRY_INTERVAL = 5 * 60 * 1_000;
 const SERVICE_WORKER_CHECK_KEY = "rabbittodo-sw-last-check";
