e97fd41b01ebebcea53c6d20b557ccd57f693126
suddenrabbit <zhoumeng1987@gmail.com>
2026-08-22 08:58:56 +0800

    fix: reduce bottom navigation height

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rabbittodo-v104";
+const CACHE = "rabbittodo-v105";
 const ASSETS = ["/", "/index.html", "/style.css", "/style-overrides.css", "/app.js", "/manifest.webmanifest", "/rabbittodo-icon.png", "/rabbittodo-icon-dock-v4.png", "/rabbittodo-avatar.png"];
 
 async function cacheLatestAssets() {
