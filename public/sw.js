0ee28f0f4d9a656bee9539164bc53d925f95fc87
suddenrabbit <zhoumeng1987@gmail.com>
2026-09-15 23:00:53 +0800

    final fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rabbittodo-v134";
+const CACHE = "rabbittodo-v135";
 const ASSETS = ["/", "/index.html", "/style.css", "/style-overrides.css", "/app.js", "/manifest.webmanifest", "/rabbittodo-icon.png", "/rabbittodo-icon-dock-v4.png", "/rabbittodo-avatar.png"];
 
 async function cacheLatestAssets() {
