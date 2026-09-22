3bfbb131abfac4305cb2293fff353863ad27520a
suddenrabbit <zhoumeng1987@gmail.com>
2026-09-22 09:53:16 +0800

    Fix standalone PWA bottom dock viewport

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rabbittodo-v139";
+const CACHE = "rabbittodo-v140";
 const ASSETS = ["/", "/index.html", "/style.css", "/style-overrides.css", "/app.js", "/manifest.webmanifest", "/rabbittodo-icon.png", "/rabbittodo-icon-dock-v4.png", "/rabbittodo-avatar.png"];
 
 async function cacheLatestAssets() {
